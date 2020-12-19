a6c1cc80494b00a1085207e037d1902ece357345
Lutz Gebelman <35678422+LutzGebelman@users.noreply.github.com>
2020-12-19 21:07:08 +0300

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,27 @@
+#include "main.cpp"
+
+void Yes() {
+    Data Vladimir;
+    srand(time(NULL));
+    system("color 70");
+    int i = rand();
+    Vladimir.name = "Vladimir";
+    Vladimir.last_name = "Sidorovich";
+    Vladimir.number = "#002342";
+    Vladimir.position = "Explorer";
+    while (true) {
+        system("cls");
+        Vladimir.Update_Data();
+
+        if (Vladimir.Temperature >= 37.5 || Vladimir.Pulse > 120 || Vladimir.blood_Saturation < 95) {
+            Vladimir.Emergency();
+        }
+        this_thread::sleep_for(chrono::seconds(3));
+    }
+}
+
+int main() {
+    thread thread1(Yes);
+    thread1.join();
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <map>
 #include <random>
 #include <thread>
 #include <chrono>
@@ -7,88 +6,51 @@
 
 using namespace std;
 
-int random_number(const int& seed){
-    srand(seed);
-    return rand();
-}
+class Data {
+public:
+    string name;
+    string last_name;
+    string position;
+    string number;
+    int Pulse;
+    double Alcohol;
+    float Temperature;
+    int Upper_Blood_Pressure;
+    int Lower_Blood_Pressure;
+    float blood_Saturation;
 
-
-struct Data {
-        string name;
-        int Pulse;
-        double Alcohol;
-        float Temperature;
-        int Upper_Blood_Pressure;
-        int Lower_Blood_Pressure;
-        float blood_Saturation;
-    };
-
-ostream& operator << (ostream& stream, Data& data){
-    return stream;
-}
-
-
-void Emergency (const Data& patient){
-    cout << "Emergency caught on " << patient.name << endl;
-    if (patient.Temperature > 37) { cout << "Temperature is: " <<  patient.Temperature << endl; }
-    if (patient.Pulse > 120) { cout << "Pulse is: " <<  patient.Pulse << endl; }
-    if (patient.blood_Saturation < 95) { cout << "Blood Saturation is: " <<  patient.blood_Saturation << endl; }
-    cout << " _______________________________________________________ " << endl;
-}
-
-void Yes(){
-    Data Vladimir;
-    srand(time(NULL));
-    while(true){
-        int i = rand();
-        Vladimir.name = "Vladimir";
-        Vladimir.Pulse = random_number(i) % 90 + 60;
-        Vladimir.Alcohol = random_number(i) / 100000.0;
-        Vladimir.Temperature = (random_number(i) % 30 + 350) / 10.0;
-        Vladimir.Upper_Blood_Pressure = random_number(i) % 50 + 100;
-        Vladimir.Lower_Blood_Pressure = Vladimir.Upper_Blood_Pressure - (random_number(i) % 9) - 40;
-        Vladimir.blood_Saturation = random_number(i) % 8 + 92;
-
-
-        if (Vladimir.Temperature >= 37 || Vladimir.Pulse > 120 || Vladimir.blood_Saturation < 95){
-            Emergency(Vladimir);
-        }
-
-
-        this_thread::sleep_for(chrono::seconds(5));
-    }
-}
-
-void Input() {
-    string input;
-    while(cin >> input) {
-        Data Vladimir;
+    void Update_Data() {
         srand(time(NULL));
-        int i = rand();
-        Vladimir.name = "Vladimir";
-        Vladimir.Pulse = random_number(i) % 90 + 60;
-        Vladimir.Alcohol = random_number(i) / 100000.0;
-        Vladimir.Temperature = (random_number(i) % 30 + 350) / 10.0;
-        Vladimir.Upper_Blood_Pressure = random_number(i) % 50 + 100;
-        Vladimir.Lower_Blood_Pressure = Vladimir.Upper_Blood_Pressure - (random_number(i) % 9) - 40;
-        Vladimir.blood_Saturation = random_number(i) % 8 + 92;
-        if (input == "Status") {
-            cout << Vladimir.name << endl
-                 << "Pulse is: " << Vladimir.Pulse << endl
-                 << "Alcohol in milliliters is: " << Vladimir.Alcohol << endl
-                 << "Temperature is: " << Vladimir.Temperature << endl
-                 << "Upper blood pressure is: " << Vladimir.Upper_Blood_Pressure << endl
-                 << "Lower blood pressure is: " << Vladimir.Lower_Blood_Pressure << endl
-                 << "Blood saturation is: " << Vladimir.blood_Saturation << endl
-                 << "_______________________________________" << endl;
-        }
+        Pulse = rand() % 90 + 60;
+        Alcohol = rand() / 100000.0;
+        Temperature = (rand() % 30 + 350) / 10.0;
+        Upper_Blood_Pressure = rand() % 50 + 100;
+        Lower_Blood_Pressure = Upper_Blood_Pressure - (rand() % 9) - 40;
+        blood_Saturation = rand() % 8 + 92;
     }
-}
 
-int main() {
-    thread thread1(Yes);
-    thread thread2(Input);
-    thread1.join();
+    void Emergency (){
+        system("color 70");
+        cout << "!!!Emergency caught!!!" << endl;
+        if (Temperature > 37.5) { cout << "Temperature is: " <<  Temperature << endl; }
+        if (Pulse > 120) { cout << "Pulse is: " <<  Pulse << endl; }
+        if (blood_Saturation < 95) { cout << "Blood Saturation is: " <<  blood_Saturation << endl; }
+        cout << "_______________________________________" << endl;
+    }
 
-    return 0;
-}
+    void Output (){
+        cout << name << endl
+            << last_name << endl
+            << position << endl
+            << number << endl << endl
+             << "Pulse is: " << Pulse << endl
+             //<< "Alcohol in milliliters is: " << Alcohol << endl
+             << "Temperature is: " << Temperature << endl
+             << "blood pressure is: " << Upper_Blood_Pressure
+             << "/"
+             << Lower_Blood_Pressure << endl
+             << "Blood saturation is: " << blood_Saturation << endl
+             << "_______________________________________" << endl;
+    }
+
+};
